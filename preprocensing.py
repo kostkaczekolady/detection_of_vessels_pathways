5a3e64f6289c002f9f7b7859445d27045ffacada
BartoszGorski <bartoszg16@gmail.com>
2018-12-29 12:29:26 +0100

    return image full path

--- preprocensing.py
+++ preprocensing.py
@@ -5,6 +5,7 @@ DRIVE_TRAIN_IMAGES = './DRIVE/training/images/'
 DRIVE_TEST_IMAGES = './DRIVE/test/images/'
     
 def get_files_list(path):
-    return [f for f in listdir(path) if isfile(join(path, f))]
+    return [join(path, f) for f in listdir(path) if isfile(join(path, f))]
 
-print(get_files_list(DRIVE_TRAIN_IMAGES))
+
+images_path = get_files_list(DRIVE_TRAIN_IMAGES)
